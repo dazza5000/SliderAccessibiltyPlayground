685c846c0e2eaf203400065fc86de70667d27f9b
Darran Kelinske <darran.kelinske@well.co>
2023-11-16 11:18:16 -0600

    yolo foreveverer

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,25 +2,35 @@
 
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Label
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.SliderState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -30,73 +40,132 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import com.example.myapplication.ui.theme.BaseWellTheme
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import com.example.myapplication.ui.theme.blueThemeColors
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
+            Scaffold(
+                topBar = { TopAppBar(title = { /*TODO*/ }) },
+                bottomBar = {
+                    BottomAppBar {
 
-
-                    var sliderPosition by remember { mutableStateOf(0f) }
-                    val sliderSize = 10f
-
-                    val labels: MutableList<String> = mutableListOf()
-                    for (i in 0..sliderSize.toInt()) {
-                        labels.add(i.toString())
                     }
+                }
+            ) {
+                MyApplicationTheme {
+                    // A surface container using the 'background' color from the theme
+                    Box {
+                        Box {
+                            Surface(
+                                modifier = Modifier.fillMaxSize(),
+                                color = MaterialTheme.colorScheme.background
+                            ) {
 
 
-                    Column(
-                        verticalArrangement = Arrangement.Center
-                    ) {
-                        WellSlider(
-                            modifier = Modifier.padding(horizontal = 20.dp),
-                            value = sliderPosition,
-                            onValueChange = { newValue: Float ->
-                                sliderPosition = newValue
-                            },
-                            labelFormatter = labels,
-                            valueRange = 0f..sliderSize,
-                            stepSize = 1f
-                        )
+                                var sliderPosition by remember { mutableStateOf(0f) }
+                                val sliderSize = 10f
+
+                                val labels: MutableList<String> = mutableListOf()
+                                for (i in 0..sliderSize.toInt()) {
+                                    labels.add(i.toString())
+                                }
+
+
+                                Column {
+                                    "foo".let {
+                                        Text(
+                                            text = it,
+                                        )
+                                    }
+                                    "bar".let {
+                                        Text(
+                                            text = it,
+                                        )
+                                    }
+
+                                    MaterialTheme(
+                                        colorScheme = MaterialTheme.colorScheme.copy(
+                                            background = Color.Magenta
+                                        )
+                                    ) {
+                                        Column(
+                                            modifier = Modifier
+                                                .fillMaxSize()
+                                                .verticalScroll(rememberScrollState())
+                                                .padding(bottom = 16.dp),
+                                                    verticalArrangement = Arrangement.Center
+                                        ) {
+                                            Row(
+                                                modifier = Modifier
+                                                    .fillMaxWidth(),
+                                                horizontalArrangement = Arrangement.SpaceBetween,
+                                            ) {
+                                                val guidanceLabels = listOf("foo", "bar")
+                                                val zeroGuidance = guidanceLabels[0]
+                                                Text(
+                                                    modifier = Modifier.semantics {
+                                                        contentDescription = "0% $zeroGuidance"
+                                                    },
+                                                    text = zeroGuidance,
+                                                )
+                                                val oneHundredGuidance = guidanceLabels[1]
+                                                Text(
+                                                    modifier = Modifier.semantics {
+                                                        contentDescription =
+                                                            "100% $oneHundredGuidance"
+                                                    },
+                                                    text = oneHundredGuidance,
+                                                )
+                                            }
+
+
+                                                WellSlider(
+                                                    modifier = Modifier.padding(horizontal = 20.dp),
+                                                    value = sliderPosition,
+                                                    onValueChange = { newValue: Float ->
+                                                        sliderPosition = newValue
+                                                    },
+                                                    labelFormatter = labels,
+                                                    valueRange = 0f..sliderSize,
+                                                    stepSize = 1f,
+                                                    colorScheme = blueThemeColors.copy(
+                                                        primary = Color.Red
+                                                    )
+                                                )
+
+
+                                                Text(
+                                                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                                                    text = sliderPosition.toString(),
+                                                )
+
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
-    }
-}
-
 
 const val WELL_SLIDER_TEST_TAG = "WELL_SLIDER_TEST_TAG"
 
@@ -109,83 +178,91 @@ fun WellSlider(
     valueRange: ClosedFloatingPointRange<Float> = 0f..1f,
     stepSize: Float = 0f,
     labelFormatter: List<String>,
+    colorScheme: ColorScheme = MaterialTheme.colorScheme
 ) {
-    val interactionSource = remember { MutableInteractionSource() }
 
-    var selectedLabel by remember {
-        mutableStateOf(
-            labelFormatter.getOrNull(value.toInt()).orEmpty()
-        )
-    }
+        BaseWellTheme(colorScheme = colorScheme) {
 
-    val thumb: @Composable (SliderState) -> Unit = {
-        Label(
-            label = {
 
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Surface(
-                        shape = RoundedCornerShape(6.dp),
-                        color = MaterialTheme.colorScheme.primary,
-                    ) {
-                        Column {
-                            Text(
-                                selectedLabel,
-                                modifier = Modifier.padding(8.dp),
-                            )
-                        }
-                    }
 
-                    Surface(
-                        modifier = Modifier
-                            .rotate(180f)
-                            .size(8.dp),
-                        shape = TriangleShape(),
-                        color = MaterialTheme.colorScheme.primary,
-                    ) {
-                    }
-                    Spacer(modifier = Modifier.padding(vertical = 4.dp))
-                }
 
-            },
-            interactionSource = interactionSource,
-        ) {
-            SliderDefaults.Thumb(
-                interactionSource = interactionSource,
-                colors = SliderDefaults.colors(),
-                enabled = enabled,
+        val interactionSource = remember { MutableInteractionSource() }
+
+        var selectedLabel by remember {
+            mutableStateOf(
+                labelFormatter.getOrNull(value.toInt()).orEmpty()
             )
         }
-    }
 
-    val steps =
-        if (stepSize == 0f) {
-            0
-        } else {
-            ((valueRange.endInclusive - valueRange.start) / stepSize + 1).toInt()
+        val thumb: @Composable (SliderState) -> Unit = {
+            Label(
+                label = {
+
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        Surface(
+                            shape = RoundedCornerShape(6.dp),
+                            color = MaterialTheme.colorScheme.primary,
+                        ) {
+                            Column {
+                                Text(
+                                    selectedLabel,
+                                    modifier = Modifier.padding(8.dp),
+                                )
+                            }
+                        }
+
+                        Surface(
+                            modifier = Modifier
+                                .rotate(180f)
+                                .size(8.dp),
+                            shape = TriangleShape(),
+                            color = MaterialTheme.colorScheme.primary,
+                        ) {
+                        }
+                        Spacer(modifier = Modifier.padding(vertical = 4.dp))
+                    }
+
+                },
+                interactionSource = interactionSource,
+            ) {
+                SliderDefaults.Thumb(
+                    interactionSource = interactionSource,
+                    colors = SliderDefaults.colors(),
+                    enabled = enabled,
+                )
+            }
         }
 
+        val steps =
+            if (stepSize == 0f) {
+                0
+            } else {
+                ((valueRange.endInclusive - valueRange.start) / stepSize + 1).toInt()
+            }
 
 
-    Slider(
-        value = value,
-        valueRange = valueRange,
-        modifier = modifier
-            .testTag(WELL_SLIDER_TEST_TAG),
-        onValueChange = {
-            onValueChange(it)
-            selectedLabel = labelFormatter.getOrNull(value.toInt()).orEmpty()
-        },
-        enabled = enabled,
-        onValueChangeFinished = {
-            // launch some business logic update with the state you hold
-            // viewModel.updateSelectedSliderValue(sliderPosition)
-        },
-        thumb = thumb,
-        interactionSource = interactionSource,
-        steps = if (stepSize == 0f) 0 else steps,
-    )
+
+        Slider(
+            value = value,
+            valueRange = valueRange,
+            modifier = modifier
+                .testTag(WELL_SLIDER_TEST_TAG),
+            onValueChange = {
+                onValueChange(it)
+                selectedLabel = labelFormatter.getOrNull(value.toInt()).orEmpty()
+            },
+            enabled = enabled,
+            onValueChangeFinished = {
+                // launch some business logic update with the state you hold
+                // viewModel.updateSelectedSliderValue(sliderPosition)
+            },
+            thumb = thumb,
+            interactionSource = interactionSource,
+            steps = if (stepSize == 0f) 0 else steps,
+        )
+    }
 }
 
 class TriangleShape : Shape {
--- app/src/main/java/com/example/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -1,11 +1,136 @@
 package com.example.myapplication.ui.theme
 
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+private val wellBlue = Color(0xff0556FF)
+private val textDark = Color(0xff1A051D)
+private val blueSteel = Color(0xff4C5870)
+private val paleSmoke = Color(0xffF5F5F5)
+private val paleAzure = Color(0xffF0F5F6)
+private val waterloo = Color(0xff7B8497)
+private val tangerine = Color(0xffED6E0C)
+private val errorLight = Color(0xffF96175)
+private val salmonMedium = Color(0xffAF2B41)
+private val whisper = Color(0xffE7E7E7)
+private val cornflowerLight = Color(0xff87C0FF)
+private val goldenrodLight = Color(0xffF8D158)
+private val seaFoam = Color(0xff48D8B2)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Color.Companion.BlueSteel: Color
+    get() = blueSteel
+val Color.Companion.WellBlue: Color
+    get() = wellBlue
+val Color.Companion.GoldenrodLight: Color
+    get() = goldenrodLight
+val Color.Companion.TextDark: Color
+    get() = textDark
+val Color.Companion.PaleAzure: Color
+    get() = paleAzure
+val Color.Companion.PaleSmoke: Color
+    get() = paleSmoke
+val Color.Companion.Waterloo: Color
+    get() = waterloo
+val Color.Companion.BlueDarkest: Color
+    get() = Color(0xff002F8C)
+val Color.Companion.BlueDarker: Color
+    get() = Color(0xff0045D9)
+val Color.Companion.BlueLighter: Color
+    get() = Color(0xff7AA7FF)
+val Color.Companion.Success: Color
+    get() = Color(0xff00C48C)
+
+val lightExtendedColors = ExtendedColors(
+    blueSteel = blueSteel,
+    waterloo = waterloo,
+    paleSmoke = paleSmoke,
+    whisper = whisper,
+    warning = tangerine,
+)
+
+val darkExtendedColors = lightExtendedColors.copy(
+    blueSteel = Color.White,
+    waterloo = Color.White,
+    paleSmoke = Color.White,
+    whisper = Color.White,
+)
+
+val lightThemeColors = lightColorScheme(
+    primary = wellBlue,
+    onPrimary = Color.White,
+    secondary = wellBlue,
+    onSecondary = Color.White,
+    secondaryContainer = Color.White,
+    onSecondaryContainer = wellBlue,
+    error = salmonMedium,
+    onBackground = textDark,
+    onSurface = textDark,
+    surfaceTint = Color.White,
+)
+
+val darkThemeColors = darkColorScheme(
+    primary = wellBlue,
+    onPrimary = Color.White,
+    secondary = wellBlue,
+    onSecondary = Color.White,
+    onSurface = Color.White,
+    onSurfaceVariant = Color.White,
+    onBackground = Color.White,
+    outline = Color.White,
+    error = salmonMedium,
+)
+
+val blueThemeColors = getThemeColors(themeColor = wellBlue, darkTheme = true)
+
+val blueSteelThemeColors = getThemeColors(themeColor = blueSteel, darkTheme = true)
+
+val seaFoamThemeColors = lightThemeColors.copy(
+    primary = textDark,
+    onPrimary = seaFoam,
+    secondary = textDark,
+    onSecondary = seaFoam,
+    surface = seaFoam,
+    background = seaFoam,
+)
+
+fun getThemeColors(
+    themeColor: Color,
+    darkTheme: Boolean = false,
+) = if (darkTheme) {
+    darkThemeColors.copy(
+        primary = Color.White,
+        onPrimary = themeColor,
+        secondary = Color.White,
+        onSecondary = themeColor,
+        surface = themeColor,
+        background = themeColor,
+    )
+} else {
+    lightThemeColors.copy(
+        primary = themeColor,
+        secondary = themeColor,
+        onSecondaryContainer = themeColor,
+    )
+}
+
+data class ExtendedColors(
+    val blueSteel: Color,
+    val waterloo: Color,
+    val paleSmoke: Color,
+    val whisper: Color,
+    val warning: Color,
+)
+
+val ColorScheme.extended: ExtendedColors
+    @Composable
+    get() = LocalExtendedColors.current
+
+val LocalExtendedColors = staticCompositionLocalOf {
+    lightExtendedColors
+}
+
